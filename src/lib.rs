eb61b1f4f06dc6f47ec8d571166e593519081a45
GaiSensei <dawnis.chow@cornell.edu>
2022-12-02 22:30:43 -0500

    trait is now defined in hexboard

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,10 @@
 //! Implements the hexagonal tiles and their properties to be used with hexboard.
 use nannou::color::encoding::Srgb;
 use nannou::color::rgb::Rgb;
-use hex2d::Coordinate;
-use nannou::prelude::*;
 
 pub mod hexagon;
 pub mod terrain;
 
 ///Type alias for nannou color type
 pub type Mrgb = Rgb<Srgb, u8>;
-
-/// Hexagonal tiles implement a trait specifying their axial location and how to draw them. 
-trait Rawr {
-    fn rawr(&self, c: Coordinate, d: &Draw, bounds: Rect);
-}
 
